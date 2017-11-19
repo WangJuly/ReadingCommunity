34fc7a094e80f2a009ad5c5f5a44b652e880bc33
WangJuly <31890313+WangJuly@users.noreply.github.com>
2017-11-19 09:49:59 +0800

    Delete admin.py

--- dushubiji/dushubiji2/booklist/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-class BookAdmin(admin.ModelAdmin):
-	list_display=('btitle','btutor')
-class UserAdmin(admin.ModelAdmin):
-	list_display=('uname','uage')
-admin.site.register(Book,BookAdmin)
-admin.site.register(User,UserAdmin)
